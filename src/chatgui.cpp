f3ed0ff9350d28b6de053db54f8501d09254d374
chaoyiwang <mithrandirwang1990@gmail.com>
2021-08-01 16:14:55 -0600

    Make _chatLogic unique ptr

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    //_chatLogic = new ChatLogic(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +135,7 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    // delete _chatLogic; // do not need to explicitly delete unique pointer
 
     ////
     //// EOF STUDENT CODE
